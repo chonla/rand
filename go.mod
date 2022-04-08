a9c9aff4d8a778055cd59fbd5f7b00705b7f56a0
Chonlasith Jucksriporn <chonlasith@gmail.com>
2022-04-08 15:59:53 +0700

    feat: Add binary randomization with selectable output format

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chonla/rand
+
+go 1.18
+
+require github.com/chonla/rnd v0.0.3
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
@@ -9,7 +11,7 @@ import (
 )
 
 // VERSION of rand
-const VERSION = "0.1.1"
+const VERSION = "0.2.0"
 
 func main() {
 	var dataSet string
@@ -17,11 +19,14 @@ func main() {
 	var ver bool
 	var help bool
 	var length int
+	var result string
+	var format string
 
 	flag.Usage = usage
 
 	flag.StringVar(&dataSet, "d", "alphanum", "set data set")
 	flag.StringVar(&entries, "e", "", "set data entries, used only when data set is custom")
+	flag.StringVar(&format, "o", "as-is", "output format")
 	flag.IntVar(&length, "l", 10, "result length")
 	flag.BoolVar(&ver, "v", false, "show rand version")
 	flag.BoolVar(&help, "h", false, "show this help")
@@ -56,6 +61,8 @@ func main() {
 		entries = rnd.HEX
 	case "hexcase":
 		entries = rnd.HEXCASE
+	case "binary":
+		result = string(rnd.Bin(length))
 	case "custom":
 		if entries == "" {
 			entries = rnd.ALPHANUM
@@ -65,13 +72,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(rnd.Of(entries, length))
+	if dataSet != "binary" {
+		result = rnd.Of(entries, length)
+	}
+
+	switch format {
+	case "hex":
+		fmt.Println(hex.EncodeToString([]byte(result)))
+	case "base64":
+		fmt.Println(base64.StdEncoding.EncodeToString([]byte(result)))
+	default:
+		fmt.Println(result)
+	}
 }
 
 func usage() {
 	fmt.Println("Usage of rand:")
 	fmt.Println()
-	fmt.Println("  rand [-d <dataset>] [-e <dataset-entries>]")
+	fmt.Println("  rand [-d <dataset>] [-e <dataset-entries>] [-o <output-format>]")
 	fmt.Println()
 	fmt.Println()
 	flag.PrintDefaults()
@@ -82,7 +100,12 @@ func usage() {
 	fmt.Println("    num : numeric (0-9)")
 	fmt.Println("    alphanum : alphanumeric (a-z + 0-9, lowercase)")
 	fmt.Println("    alphanumcase : alphanumeric with case-sensitive (a-z + A-Z + 0-9)")
-	fmt.Println("    hex : hexeric (0-9 + a-f, lowercase)")
-	fmt.Println("    hexcase : hexeric with case-sensitive (0-9 + a-f + A-F)")
+	fmt.Println("    hex : hexadecimal (0-9 + a-f, lowercase)")
+	fmt.Println("    hexcase : hexadecimal with case-sensitive (0-9 + a-f + A-F)")
+	fmt.Println("    binary : binary string")
 	fmt.Println("    custom : custom data set, use -e to specify entries in data set")
+	fmt.Println()
+	fmt.Println("  possible alternative output format:")
+	fmt.Println("    base64 : encode randomized string into base64 string")
+	fmt.Println("    hex : encode randomized string into hexadecimal string")
 }
